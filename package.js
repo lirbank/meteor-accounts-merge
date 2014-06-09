d5ce2cb83e2a09e8592cc10804f253945af025fe
Mikael Lirbank <mikael@lirbank.com>
2014-06-09 15:30:30 -0700

    Add onMerge hook

--- package.js
+++ package.js
@@ -3,4 +3,5 @@ Package.on_use(function (api) {
   api.use(['accounts-base'], 'server');
   api.add_files('accounts-merge-server.js', 'server');
   api.add_files('accounts-merge-client.js', 'client');
+  api.export('AccountsMerge');
 });
