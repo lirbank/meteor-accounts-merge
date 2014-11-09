8258e8129680facc28d396f0892636c774769141
Urs Lerch <mail@ulerch.net>
2014-11-09 17:25:31 -0500

    Added Soundcloud service to the server

--- accounts-merge-server.js
+++ accounts-merge-server.js
@@ -19,7 +19,7 @@ Meteor.methods({
     var oldAccount = Meteor.users.findOne(oldAccountId);
     var newAccount = Meteor.users.findOne(this.userId);
 
-    _services = [ "facebook", "twitter", "google", "linkedin", "github" ];
+    _services = [ "facebook", "twitter", "google", "linkedin", "github", "soundcloud" ];
 
     // Move login services from loosing to winning user
     for (i=0; i<_services.length; i++) {
